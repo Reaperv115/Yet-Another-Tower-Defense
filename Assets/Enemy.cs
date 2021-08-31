8c1fb0a9467f2bfa81e6b747a1a3e97d868f0bfb
Reaper <rjs5785@hotmail.com>
2021-08-31 12:21:22 -0400

    enemies move towards tower to attack it once they reach the final path marker

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -16,6 +16,7 @@ public class Enemy : MonoBehaviour
     GameObject tower;
     Tower towerS;
 
+    bool attackTower;
 
     Rigidbody2D enemyRb;
 
@@ -25,35 +26,45 @@ public class Enemy : MonoBehaviour
         mapInfo = cam.GetComponent<SetMap>();
         enemyRb = GetComponent<Rigidbody2D>();
         towerS = tower.GetComponent<Tower>();
+        attackTower = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (mapInfo.path.Length.Equals(0))
-        {
-            mapInfo.path = Resources.LoadAll<GameObject>("Path");
-            transform.position = Vector3.MoveTowards(transform.position, mapInfo.path[pathIndex].transform.position, 0.02f);
-        }
+        if (attackTower)
+            transform.position = Vector3.MoveTowards(transform.position, tower.transform.position, .02f);
         else
         {
 
-            if (transform.position.Equals(mapInfo.path[pathIndex].transform.position))
-            {
-                if (pathIndex.Equals(mapInfo.path.Length))
-                {
-                    transform.position = Vector3.MoveTowards(transform.position, tower.transform.position, .02f);
-                }
-                else
-                {
-                    ++pathIndex;
-                    transform.position = Vector3.MoveTowards(transform.position, mapInfo.path[pathIndex].transform.position, .02f);
-                }
 
+            if (mapInfo.path.Length.Equals(0))
+            {
+                mapInfo.path = Resources.LoadAll<GameObject>("Path");
+                transform.position = Vector3.MoveTowards(transform.position, mapInfo.path[pathIndex].transform.position, 0.02f);
             }
             else
             {
-                transform.position = Vector3.MoveTowards(transform.position, mapInfo.path[pathIndex].transform.position, .02f);
+                if (transform.position.Equals(mapInfo.path[pathIndex].transform.position))
+                {
+                    Debug.Log("path index: " + pathIndex);
+                    //Debug.Log("path length: " + mapInfo.path.Length);
+                    if (pathIndex.Equals(mapInfo.path.Length - 1))
+                    {
+                        attackTower = true;
+                        //transform.position = Vector3.MoveTowards(transform.position, tower.transform.position, .02f);
+                    }
+                    else
+                    {
+                        ++pathIndex;
+                        transform.position = Vector3.MoveTowards(transform.position, mapInfo.path[pathIndex].transform.position, .02f);
+                    }
+
+                }
+                else
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, mapInfo.path[pathIndex].transform.position, .02f);
+                }
             }
         }
 
@@ -63,10 +74,4 @@ public class Enemy : MonoBehaviour
     {
         return tower;
     }
-
-    //public void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    Debug.Log(collision.transform.name);
-    //    
-    //}
 }
