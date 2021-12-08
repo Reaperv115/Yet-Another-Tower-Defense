426eac4da211975a6643c2cdf66c2a1e9dfd4f30
rjs5785 <rjs5785@hotmail.com>
2021-12-08 09:01:58 -0500

    weapons can now shoot and destroy enemies when they are close enough

--- Assets/ShootEnemy.cs
+++ Assets/ShootEnemy.cs
@@ -5,38 +5,46 @@ using UnityEngine;
 public class ShootEnemy : MonoBehaviour
 {
     Vector3 offSet;
-    Ray2D ray;
-    RaycastHit2D hit;
-    float vision = 5f;
-    LayerMask mask;
+    Collider2D collider;
 
     // Start is called before the first frame update
     void Start()
     {
-        mask = LayerMask.GetMask("enemy");
-        Debug.Log(mask.value);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //hit = Physics2D.Raycast(transform.position, transform.right * vision, mask);
-        //Debug.DrawRay(transform.position, transform.right * vision);
-        //if (hit.collider.tag.Equals("enemy"))
-        //{
-        //    hit.collider.GetComponent<Enemy>().Health -= 25f;
-        //}
     }
 
     public void OnTriggerStay2D(Collider2D collision)
     {
-        offSet = collision.transform.position - transform.position;
-        Debug.Log(collision.transform.name);
         if (collision.transform.tag.Equals("enemy"))
         {
+            collider = collision;
+            offSet = collision.transform.position - transform.position;
             Quaternion rotation = Quaternion.LookRotation(Vector3.forward, offSet);
             transform.rotation = rotation * Quaternion.Euler(0, 0, 90);
+            Invoke("Fire", .25f);
         }
         
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag.Equals("enemy"))
+        {
+            collider = collision;
+            offSet = collision.transform.position - transform.position;
+            Quaternion rotation = Quaternion.LookRotation(Vector3.forward, offSet);
+            transform.rotation = rotation * Quaternion.Euler(0, 0, 90);
+            Invoke("Fire", .25f);
+        }
+    }
+
+    void Fire()
+    {
+        //Destroy(collider.gameObject);
+        collider.GetComponent<Enemy>().Health -= 25f;
     }
 }
