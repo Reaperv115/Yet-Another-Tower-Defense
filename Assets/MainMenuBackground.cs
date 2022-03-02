680bed58bbcdbcf5d52b18076c856eff2067438f
rjs5785 <rjs5785@hotmail.com>
2022-03-02 12:33:36 -0500

    created a main menu with a start button that loads the actual game scene. I have the title and background changing and alternating colors, just to catch the players eye and attention

---
+++ Assets/MainMenuBackground.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MainMenuBackground : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI Title;
+    Color[] backgroundColors = { Color.black, Color.green, Color.blue, Color.yellow, Color.red };
+    float colorTimer;
+    int newtmp;
+    int tmp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tmp = Random.Range(0, backgroundColors.Length);
+        newtmp = tmp + 3;
+        if (newtmp > backgroundColors.Length)
+        {
+            newtmp = (tmp + 3) - backgroundColors.Length;
+        }
+        GetComponent<SpriteRenderer>().color = backgroundColors[tmp];
+        Title.color = backgroundColors[newtmp];
+        colorTimer = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (colorTimer <= 0f)
+        {
+            tmp = Random.Range(0, backgroundColors.Length);
+            newtmp = tmp + 3;
+            if (newtmp > backgroundColors.Length)
+            {
+                newtmp = (tmp + 3) - backgroundColors.Length;
+            }
+            GetComponent<SpriteRenderer>().color = backgroundColors[tmp];
+            Title.color = backgroundColors[newtmp];
+            colorTimer = 5f;
+        }
+        else
+        {
+            Debug.Log(colorTimer);
+            colorTimer -= Time.deltaTime;
+        }
+    }
+}
--- Assets/Scripts/EnemyBase.cs
+++ Assets/Scripts/EnemyBase.cs
@@ -6,14 +6,9 @@ using UnityEngine.UI;
 
 public class EnemyBase : MonoBehaviour
 {
-    //[SerializeField]
-    //GameObject cam;
-    //SetMap mapInfo;
 
     protected int pathIndex;
     protected int pathindexPoint;
-
-    //GameObject tower;
 
     protected bool attackTower;
 
@@ -23,64 +18,13 @@ public class EnemyBase : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //mapInfo = GameObject.Find("battlefield").GetComponent<SetMap>();
-        //attackTower = false;
-        //Health = 100;
-        //speed = 10f * Time.deltaTime;
+        
     }
 
     // Update is called once per frame
     void Update()
-    {
-        //if (attackTower)
-        //{
-        //    tower = GameObject.FindGameObjectWithTag("tower");
-        //    transform.position = Vector3.MoveTowards(transform.position, tower.transform.position, speed);
-        //}
-        //else
-        //{
-        //    Go(speed);
-
-
-        //}
-
-    }
-
-    //void Go(float _speed)
-    //{
-    //    if (transform.position.Equals(mapInfo.pathwayMarkers[pathIndex].transform.GetChild(pathindexPoint).position))
-    //    {
-    //        if (pathindexPoint.Equals(3))
-    //        {
-    //            if (pathIndex.Equals(mapInfo.pathwayMarkers.Length - 1))
-    //            {
-    //                attackTower = true;
-
-    //            }
-    //            else
-    //            {
-    //                ++pathIndex;
-    //                pathindexPoint = 2;
-    //                transform.position = Vector3.MoveTowards(transform.position, mapInfo.pathwayMarkers[pathIndex].transform.GetChild(pathindexPoint).position, _speed);
-    //            }
-    //        }
-    //        else
-    //        {
-    //            ++pathindexPoint;
-    //            transform.position = Vector3.MoveTowards(transform.position, mapInfo.pathwayMarkers[pathIndex].transform.GetChild(pathindexPoint).position, _speed);
-    //        }
-
-    //    }
-    //    else
-    //    {
-    //        transform.position = Vector3.MoveTowards(transform.position, mapInfo.pathwayMarkers[pathIndex].transform.GetChild(pathindexPoint).position, _speed);
-    //    }
-    //}
-
-    //public GameObject getTower()
-    //{
-    //    return tower;
-    //}
+    { }
+        
 
     public int Health { get; set; }
 }
